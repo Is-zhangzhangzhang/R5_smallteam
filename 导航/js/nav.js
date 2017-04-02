ffd8dbcf6d280c256b54f4a183791e56055d4d00
Is-zhangzhangzhang <Is_zhangjiayi@163.com>
2017-04-03 01:02:49 +0800

    remove 导航

--- 导航/js/nav.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-         $(".Top_right_nav a").mouseenter(function(){
-             // $(".Top_nav_list").css("display",'block');
-             $(".Top_nav_list").fadeIn('slow');
-         });
-        $(".Top_right_nav a").mouseleave(function(){
-             $(".Top_nav_list").fadeOut('slow');
-        });
-});
